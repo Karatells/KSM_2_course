54c29f4f9c8399d836377327d7d5fda0f32a4c6b
Falcofemoralis <araraunapappagallo@gmail.com>
2020-03-02 21:19:22 +0200

    Update Lessons_schedule.java

--- java/com/example/ksm_2_course/Lessons_schedule.java
+++ java/com/example/ksm_2_course/Lessons_schedule.java
@@ -2,7 +2,6 @@ package com.example.ksm_2_course;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
@@ -25,7 +24,6 @@ public class Lessons_schedule extends AppCompatActivity {
     static  String FILE_NAME;
     int first_day=2 ;
     static int week;
-    static int endTimeM,endTimeH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +31,7 @@ public class Lessons_schedule extends AppCompatActivity {
         setContentView(R.layout.activity_lessons_schedule);
 
         //////////////////////////////////////////////
-                      //Обводка//
+        //Обводка//
         //////////////////////////////////////////////
 
         //получаем день недели (пн,вт и пр)
@@ -47,60 +45,60 @@ public class Lessons_schedule extends AppCompatActivity {
         tableLayout_r[3] = (TableLayout) findViewById(R.id.tableLayout_thur);
 
         //при совпадении дней делаем обводку красной (т.е меняем файл с noborders на borders)
-        if(dayOfWeek==2){
+        if (dayOfWeek == 2) {
             tableLayout_r[0].setBackground(getResources().getDrawable(R.drawable.borders));
-        }else if(dayOfWeek==3){
+        } else if (dayOfWeek == 3) {
             tableLayout_r[1].setBackground(getResources().getDrawable(R.drawable.borders));
-        }else if(dayOfWeek==4){
+        } else if (dayOfWeek == 4) {
             tableLayout_r[2].setBackground(getResources().getDrawable(R.drawable.borders));
-        }else if(dayOfWeek==5){
+        } else if (dayOfWeek == 5) {
             tableLayout_r[3].setBackground(getResources().getDrawable(R.drawable.borders));
         }
 
         //////////////////////////////////////////////
-                        //Данные в таблице//
+        //Данные в таблице//
         //////////////////////////////////////////////
 
-       //выбираю базу данных
-        if(MainActivity.StatusButton==0){
+        //выбираю базу данных
+        if (MainActivity.StatusButton == 0) {
             FILE_NAME = "knt518.json";
-        }else if(MainActivity.StatusButton==1){
+        } else if (MainActivity.StatusButton == 1) {
             FILE_NAME = "knt528.json";
         }
 
         //создаю обьекты таблицы
         int id;
 
-        String table1 = "table",stringId;
-        for(int i=0;i<4;i++){
-            for(int j=1;j<4;j++){
+        String table1 = "table", stringId;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 1; j < 4; j++) {
                 stringId = table1 + Integer.toString(i) + "_" + Integer.toString(j);
                 id = getResources().getIdentifier(stringId, "id", getApplicationContext().getPackageName());
                 textViewMonday[i][j] = (TextView) findViewById(id);
             }
         }
 
-        String table2 = "table2",stringId2;
-        for(int i=0;i<4;i++){
-            for(int j=1;j<4;j++){
+        String table2 = "table2", stringId2;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 1; j < 4; j++) {
                 stringId2 = table2 + Integer.toString(i) + "_" + Integer.toString(j);
                 id = getResources().getIdentifier(stringId2, "id", getApplicationContext().getPackageName());
                 textViewTuesday[i][j] = (TextView) findViewById(id);
             }
         }
 
-        String table3 = "table3",stringId3;
-        for(int i=0;i<4;i++){
-            for(int j=1;j<4;j++){
+        String table3 = "table3", stringId3;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 1; j < 4; j++) {
                 stringId3 = table3 + Integer.toString(i) + "_" + Integer.toString(j);
                 id = getResources().getIdentifier(stringId3, "id", getApplicationContext().getPackageName());
                 textViewWednesday[i][j] = (TextView) findViewById(id);
             }
         }
 
-        String table4 = "table4",stringId4;
-        for(int i=0;i<4;i++){
-            for(int j=1;j<4;j++){
+        String table4 = "table4", stringId4;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 1; j < 4; j++) {
                 stringId4 = table4 + Integer.toString(i) + "_" + Integer.toString(j);
                 id = getResources().getIdentifier(stringId4, "id", getApplicationContext().getPackageName());
                 textViewThursday[i][j] = (TextView) findViewById(id);
@@ -112,117 +110,22 @@ public class Lessons_schedule extends AppCompatActivity {
         int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
         TextView nameOfWeek = (TextView) findViewById(R.id.nameOfWeek);
         Button changeScheduleButton = (Button) findViewById(R.id.changeScheduleButton);
-        if(dayOfMonth==1){
-            week=1;
+        if (dayOfMonth == 1) {
+            week = 1;
             nameOfWeek.setText("ЧИСЕЛЬНИК");
             changeScheduleButton.setText("ЗНАМЕННИК");
-        }else if(((dayOfMonth-first_day)/7)%2==0){
-            week=0;
+        } else if (((dayOfMonth - first_day) / 7) % 2 == 0) {
+            week = 0;
             nameOfWeek.setText("ЗНАМЕННИК");
             changeScheduleButton.setText("ЧИСЕЛЬНИК");
-        }else{
-            week=1;
+        } else {
+            week = 1;
             nameOfWeek.setText("ЧИСЕЛЬНИК");
             changeScheduleButton.setText("ЗНАМЕННИК");
         }
 
         setDataBase();
 
-        //////////////////////////////////////////////
-                        //Таймер//
-        //////////////////////////////////////////////
-        int currentTimeH = calendar.get(Calendar.HOUR_OF_DAY),currentTimeM = calendar.get(Calendar.MINUTE),currentTimeS = calendar.get(Calendar.SECOND);
-        int currentTime = currentTimeH*60+currentTimeM;
-        TextView timeUntil = (TextView) findViewById(R.id.timeUntil);
-
-        //начало и конец пары (в минутах)
-        int[][] lessons = { { 510, 590 }, { 605, 685 }, { 715, 795}, { 805, 885}, { 895, 975 } };
-
-        //нахожу какая сейчас пара
-        //endTimeM времени осталось до конца пары (в минутах)
-        if(currentTime>=lessons[0][0] && currentTime<lessons[0][1]){
-            endTimeM = lessons[0][1]-currentTime;
-            time(currentTimeS);
-
-        }else if(currentTime>=lessons[1][0] && currentTime<lessons[1][1]){
-            endTimeM = lessons[1][1]-currentTime;
-            time(currentTimeS);
-
-        }else if(currentTime>=lessons[2][0] && currentTime<lessons[2][1]){
-            endTimeM = lessons[2][1]-currentTime;
-            time(currentTimeS);
-
-        }else if(currentTime>=lessons[3][0] && currentTime<lessons[3][1]){
-            endTimeM = lessons[3][1]-currentTime;
-            time(currentTimeS);
-
-        }else if(currentTime>=lessons[4][0] && currentTime<lessons[4][1]){
-            endTimeM = lessons[4][1]-currentTime;
-            time(currentTimeS);
-
-        }else if(currentTime>=lessons[0][1] && currentTime<lessons[1][0]){
-            timeUntil.setText("Початок II пари:");
-            endTimeM = lessons[1][0]-currentTime;
-            time(currentTimeS);
-
-        }else if(currentTime>=lessons[1][1] && currentTime<lessons[2][0]){
-            timeUntil.setText("Початок III пари:");
-            endTimeM = lessons[2][0]-currentTime;
-            time(currentTimeS);
-
-        }else if(currentTime>=lessons[2][1] && currentTime<lessons[3][0]){
-            timeUntil.setText("Початок IV пари:");
-            endTimeM = lessons[3][0]-currentTime;
-            time(currentTimeS);
-
-        }else if(currentTime>=lessons[3][1] && currentTime<lessons[4][0]){
-            timeUntil.setText("Початок V пари:");
-            endTimeM = lessons[4][0]-currentTime;
-            time(currentTimeS);
-
-        }else{
-            timeUntil.setText("Початок I пари:");
-            if(currentTime>lessons[0][0]) endTimeM = 24*60-currentTime+lessons[0][0];
-            else endTimeM = lessons[0][0]-currentTime;
-            time(currentTimeS);
-        }
-    }
-
-    public void time(int currentTime){
-        TextView textView12 = (TextView) findViewById(R.id.textView12);
-        endTimeH=endTimeM/60;
-        endTimeM%=60;
-        --endTimeM;
-        if(endTimeH==0) textView12.setText(" ");
-        timer((60-currentTime)*1000);
-    }
-    public void timer(int seconds){
-        final TextView timerS = (TextView) findViewById(R.id.timerS);
-        TextView timerM = (TextView) findViewById(R.id.timerM);
-        TextView timerH = (TextView) findViewById(R.id.timerH);
-        timerM.setText(Integer.toString(endTimeM));
-        if(endTimeH!=0) timerH.setText(Integer.toString(endTimeH));
-        new CountDownTimer(seconds, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                if(millisUntilFinished<10){
-                    timerS.setText(""  + millisUntilFinished/1000);
-                }else{
-                    timerS.setText("" + millisUntilFinished/1000);
-                }
-
-            }
-
-            @Override
-            public void onFinish() {
-                --endTimeM;
-                if(endTimeM>0) timer(60000);
-                else if(endTimeH>0){
-                    --endTimeH;
-                    endTimeM=60;
-                }
-            }
-        }.start();
     }
 
     public void OnClick(View v)
